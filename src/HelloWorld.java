cb951f76ce14614afd04b131a2ef0fc93717d9f1
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-01-22 09:24:56 -0600

    syntax

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,30 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 47;
+        System.out.println("My favorite number is " + myFavoriteNumber);
+        String myString = "I love this string!";
+        System.out.println(myString);
+
+        myNumber = 123;
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+
+
     }
 }
