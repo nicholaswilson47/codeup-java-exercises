8a24df0a217b70228f6da8b5374b69f239f541fb
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-01-25 15:33:29 -0600

    4 steps on string ex

---
+++ src/StringExercise.java
@@ -0,0 +1,14 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String output ="We don't need no education\n We don't need no thought control";
+        System.out.println(output);
+
+        String output2 = "Check \"this\" out!, \"s inside of \" s";
+        System.out.println(output2);
+
+        String output3 = "In windows, the main drive is usually C:\\";
+        System.out.println(output3);
+
+        String output4 = "I can do backslashes \\, double backslashes \\\\, \n and the amazing triple backslash \\\\\\!";
+    }
+}
