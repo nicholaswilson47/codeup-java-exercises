dd1b2b2fc8e003a2b7b2f4e701d4c8609ec3a8d4
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-01-26 13:53:31 -0600

    working on method ex1

---
+++ src/MethodExercises.java
@@ -0,0 +1,38 @@
+public class MethodExercises {
+
+    public static int addNumbers(int num1, int num2){
+        return num1 + num2;
+    }
+    public static int subtractNumbers(int num1, int num2){
+        return num1 - num2;
+    }
+    public static int multiplyNumbers(int num1, int num2){
+        return num1 * num2;
+    }
+    public static int divideNumbers(int num1, int num2){
+        return num1 / num2;
+    }
+    public static int getInteger(int min, int max){
+
+    };
+
+    public static void main(String[] args) {
+        System.out.println(addNumbers(4,2));
+        System.out.println(subtractNumbers(4,2));
+        System.out.println(multiplyNumbers(4,2));
+        System.out.println(divideNumbers(4,2));
+    }
+}
+//    Basic Arithmetic
+//
+//    Create four separate methods. Each will perform an arithmetic operation:
+//
+//        Addition
+//        Subtraction
+//        Multiplication
+//        Division
+//        Each function needs to take two parameters/arguments so that the operation can be performed.
+//
+//        Test your methods and verify the output.
+//
+//        Add a modulus method that finds the modulus of two numbers.
