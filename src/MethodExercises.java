d5a8e913911163fb156dee8216b5beee14a6bd7e
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-01-27 10:29:40 -0600

    more method ex

--- src/MethodExercises.java
+++ src/MethodExercises.java
@@ -1,26 +1,63 @@
+import java.util.Scanner;
+
 public class MethodExercises {
 
-    public static int addNumbers(int num1, int num2){
+    public static double addNumbers(double num1, double num2){
         return num1 + num2;
     }
-    public static int subtractNumbers(int num1, int num2){
+    public static double subtractNumbers(double num1, double num2){
         return num1 - num2;
     }
-    public static int multiplyNumbers(int num1, int num2){
+    public static double multiplyNumbers(double num1, double num2){
         return num1 * num2;
     }
-    public static int divideNumbers(int num1, int num2){
+    public static double divideNumbers(double num1, double num2){
+        if (num2 == 0){
+            System.out.println("Your answer is undefined");
+            return 0;
+        }
         return num1 / num2;
     }
+    public static double modulusNumbers(double num1, double num2){
+        return num1 % num2;
+    }
     public static int getInteger(int min, int max){
-
+        Scanner input = new Scanner(System.in);
+        int userInput = input.nextInt();
+        if (userInput >= min && userInput <= max){
+            System.out.println("You chose a good number");
+            return userInput;
+        } else {
+            System.out.println("that number was out of range");
+            System.out.println("try again");
+            return getInteger(min,max);
+        }
     };
+    public static long getFactorial(long num){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter another number between 1 and 10: ");
+        long userInput = input.nextInt();
+        long factorial = 1;
+        if (userInput > 10 || userInput < 1 ){
+            return getFactorial(userInput);
+        } else {
+            for (long i =1; i <= userInput; i++){
+                factorial = factorial * i;
+                System.out.println("!" + i + " = " + factorial);
+            }
+        }
+        System.out.println("Continue?");
+        return userInput;
+    }
 
     public static void main(String[] args) {
         System.out.println(addNumbers(4,2));
         System.out.println(subtractNumbers(4,2));
         System.out.println(multiplyNumbers(4,2));
         System.out.println(divideNumbers(4,2));
+        System.out.println(modulusNumbers(4,2));
+        System.out.println(getFactorial(5));
+
     }
 }
 //    Basic Arithmetic
