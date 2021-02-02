8434e96fb813d881624dd12188d3988af39bb26f
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-02-02 09:52:31 -0600

    fixing input

--- src/Person.java
+++ src/Person.java
@@ -1,13 +1,46 @@
-public class Person {
+class Person {
     private String name;
+
+    Person(String name){
+        this.name = name;
+    }
+
     public String getName() {
 //TODO: return the person's name
+        return this.name;
     }
 
     public void setName(String name){
 //TODO: change the name property to the passed value
+        this.name = name;
     }
     public void sayHello(){
 //TODO: print a message to the console using the person's name
+        System.out.println("Hello there, " + this.name);
     }
+
+    public static void main(String[] args){
+        Person nick = new Person("Nick");
+        System.out.println(nick.getName());
+        nick.setName("Nicholas");
+        nick.sayHello();
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+    }
+
 }
