5ff07fa25a98a45fd594701d7c4c1fe734100a7d
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-02-28 13:15:17 -0600

    added square

---
+++ src/Marker.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Marker {
+    public static void main(String[] args) {
+        String[] names = {"James Gosling", "Mike Sheridan", "Patrick Naughton"};
+        for (String name : names)
+            System.out.println(name);
+    }
+    }
+}
