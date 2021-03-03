4be730f2980311ddd028ec1433115209bfa1c945
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-03-03 11:25:27 -0600

    remove other stuff

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,28 +1,21 @@
 package shapes;
 
-public class Rectangle extends Quadrilateral implements Measurable{
 
+
+public class Rectangle extends Quadrilateral implements Measurable {
     public Rectangle(double length, double width) {
         super(length, width);
+
     }
 
     @Override
     public double getPerimeter() {
-        return 2 * (length + width);
+        return 2 *length + 2* width;
     }
 
     @Override
     public double getArea() {
-        return length * width;
+        return length*width;
     }
-
-    @Override
-    void setLength(double length) {
-        this.length = length;
-    }
-
-    @Override
-    void setWidth(double width) {
-        this.width = width;
-    }
+//
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,29 +1,23 @@
 package shapes;
 
+
+
 public class Square extends Quadrilateral {
+    protected double side;
+
 
     public Square(double side) {
         super(side, side);
     }
 
+
     @Override
     public double getPerimeter() {
-        return 4 * length;
+        return 4*length;
     }
 
     @Override
     public double getArea() {
-        return Math.pow(length,2);
-    }
-
-    @Override
-    void setLength(double length) {
-        this.length = length;
-        this.width = length;
-    }
-
-    @Override
-    void setWidth(double width) {
-        this.length = this.width = width;
+        return length*length;
     }
 }
